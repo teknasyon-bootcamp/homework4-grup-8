de3fd3e970a510efe4c0979fecc7ce7fb6d627da
Hasan UYANIK <40677383+hasanuyanik@users.noreply.github.com>
2021-09-09 00:27:56 +0300

    DB işlemlerinin DB.class ile yapılacak şekilde düzenlenmesi

--- post.class.php
+++ post.class.php
@@ -13,47 +13,47 @@ class Post{
 
     public function PostList(){
         try {
-            $posts = $this->DB->pdo->query("SELECT * FROM posts", PDO::FETCH_OBJ); // DB nesnesi yoluyla PDO query ile posts tablosunu objeler olarak getirmek için ayarlamaları yapıp $posts'a ata.
+            $posts = $this->DB->CRUD(table: "posts", CrudType: "SELECT"); // DB nesnesi yoluyla posts tablosundaki tüm verileri getirmek için ayarlamaları yapıp $post'a ata.
             $posts->execute(); // $posts daki işlemi çalıştır
             return $posts->fetchAll(); // $posts ile gelen tüm verileri getirip fonksiyonun çağrıldığı yere gönder
-        }catch (PDOException $exception) { // PDO işlemleri hata verirse yakala ve exception değişkeniyle değer döndür
+        }catch (Exception $exception) { // PDO işlemleri hata verirse yakala ve exception değişkeniyle değer döndür
             return $exception; // Fonksiyonun çağrıldığı yere hatayı gönder
         }
     }
 
     public function PostInfo(int $id){
         try {
-            $post = $this->DB->pdo->prepare("SELECT * FROM posts WHERE id=:id"); // DB nesnesi yoluyla PDO prepare ile posts tablosundaki istenen id değerli veriyi getirmek için ayarlamaları yapıp $post'a ata.
+            $post = $this->DB->CRUD(table:"posts",CrudType: "SELECT",where: "id=:id"); // DB nesnesi yoluyla posts tablosundaki istenen id değerli veriyi getirmek için ayarlamaları yapıp $post'a ata.
             $post->execute([":id" => $id]); // $post daki işlem için id değerini kullan ve çalıştır
             return $post->fetch(); // $post ile gelen veriyi fonksiyonun çağrıldığı yere gönder
-        }catch (PDOException $exception) {  // PDO işlemleri hata verirse yakala ve exception değişkeniyle değer döndür
+        }catch (Exception $exception) {  // PDO işlemleri hata verirse yakala ve exception değişkeniyle değer döndür
             return $exception; // Fonksiyonun çağrıldığı yere hatayı gönder
         }
     }
 
     public function CreatePost(string $header, string $content){
         try {
-            $createPost = $this->DB->pdo->prepare("INSERT INTO posts(header,content) value(:header,:content)"); // DB nesnesi yoluyla PDO prepare ile posts tablosuna yeni veri girmek için ayarlamaları yapıp $createPost'a ata.
+            $createPost = $this->DB->CRUD(table: "posts",CrudType: "INSERT",insertColumns: "header,content",insertValues: ":header,:content"); // DB nesnesi yoluyla posts tablosuna yeni veri girmek için ayarlamaları yapıp $createPost'a ata.
             return $createPost->execute([":header" => $header, ":content" => $content]); // $createPost daki işlem için header ve content değerlerini kullan ve çalıştır
-        }catch (PDOException $exception) { // PDO işlemleri hata verirse yakala ve exception değişkeniyle değer döndür
+        }catch (Exception $exception) { // PDO işlemleri hata verirse yakala ve exception değişkeniyle değer döndür
             return $exception; // Fonksiyonun çağrıldığı yere hatayı gönder
         }
     }
 
     public function EditPost(int $id, string $header, string $content){
         try {
-            $editPost = $this->DB->pdo->prepare("UPDATE posts SET header=:header, content=:content WHERE id=:id"); // DB nesnesi yoluyla PDO prepare ile posts tablosundaki istenen id değerli veriyi güncellemek için ayarlamaları yapıp $editPost'a ata.
+            $editPost = $this->DB->CRUD(table: "posts",CrudType: "UPDATE",updateSet: "header=:header, content=:content",where: "id=:id"); // DB nesnesi yoluyla posts tablosundaki istenen id değerli veriyi güncellemek için ayarlamaları yapıp $editPost'a ata.
             return $editPost->execute([":id" => $id,":header" => $header, ":content" => $content]); // $editPost daki işlem için id, header ve content değerlerini kullan ve çalıştır
-        }catch (PDOException $exception) { // PDO işlemleri hata verirse yakala ve exception değişkeniyle değer döndür
+        }catch (Exception $exception) { // PDO işlemleri hata verirse yakala ve exception değişkeniyle değer döndür
             return $exception; // Fonksiyonun çağrıldığı yere hatayı gönder
         }
     }
 
     public function DeletePost(int $id){
         try {
-            $deletePost = $this->DB->pdo->prepare("DELETE FROM posts WHERE id=:id"); // DB nesnesi yoluyla PDO prepare ile posts tablosundaki istenen id değerli veriyi silmek için ayarlamaları yapıp $deletePost'a ata.
+            $deletePost = $this->DB->CRUD(table: "posts",CrudType: "DELETE",where: "id=:id"); // DB nesnesi yoluyla posts tablosundaki istenen id değerli veriyi silmek için ayarlamaları yapıp $deletePost'a ata.
             return $deletePost->execute([":id" => $id]); // $deletePost taki işlem için id değerini kullan ve çalıştır
-        }catch (PDOException $exception) { // PDO işlemleri hata verirse yakala ve exception değişkeniyle değer döndür
+        }catch (Exception $exception) { // PDO işlemleri hata verirse yakala ve exception değişkeniyle değer döndür
             return $exception; // Fonksiyonun çağrıldığı yere hatayı gönder
         }
     }
